c6fe6538ab268882b2cd7cf75f8a774536824251
Taha Anwar <123452083+tahaanwar31@users.noreply.github.com>
2023-02-01 23:59:53 +0500

    basic bubble sort program using repeat until loop

---
+++ ConsoleApplication1/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name() = {"Taha", "Hiba", "Alishba", "Tahreem", "Sara", "Ehsaan", "Areesha", "Saira", "Asmaar", "Rayyan", "Anaya", "Sidra", "Samreena", "Daniyal"}
+        Dim count, flag, x As Integer
+        Dim temp As String
+        Do 'Repeat
+            count = 0
+            flag = 0
+            Do 'Repeat
+                If name(count) > name(count + 1) Then
+                    temp = name(count)
+                    name(count) = name(count + 1)
+                    name(count + 1) = temp
+                    flag = 1
+                End If
+                count = count + 1
+            Loop Until count = 13
+        Loop Until flag = 0
+        For x = 0 To 13
+            Console.WriteLine(name(x))
+        Next
+
+        Console.ReadKey()
+    End Sub
+
+End Module
